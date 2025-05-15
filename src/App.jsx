61fbfeaf1f7afff6824fe00069ef9b881956e4bd
amalia barrigas <amaliabarrigas@gmail.com>
2025-05-15 15:57:50 +0200

    ejercicio terminado

--- src/App.jsx
+++ src/App.jsx
@@ -5,19 +5,25 @@ import StudentDetailsPage from "./pages/StudentDetailsPage";
 import UserProfilePage from "./pages/UserProfilePage";
 
 
+import { Routes, Route } from "react-router-dom";
+
+
 function App() {
 
   return (
     <div className="App relative z-20 pt-20">
       <Navbar />
-
+      
       <div className="pages">
-        <HomePage />
+        <Routes>
+        <Route path="/" element={<HomePage />} /> 
 
-        <StudentDetailsPage />
+        <Route path="/students/:studentId" element={<StudentDetailsPage />} /> 
 
-        <UserProfilePage />
+        <Route path="/profile" element={<UserProfilePage />} /> 
+        </Routes>
       </div>
+      
 
     </div>
   );
